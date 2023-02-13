7a22f0afb8ee30a911f1fbd36e112b694d9958de
k15 <krisnantobiyuh@gmail.com>
2023-02-13 16:17:59 +0700

    Fix(Exception): Update 400 bad request

--- src/Exception/MootaCore400Exception.php
+++ src/Exception/MootaCore400Exception.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Otnansirk\Moota\Exception;
 
 use LogicException;
+use Illuminate\Validation\Validator;
 
 class MootaCore400Exception extends LogicException
 {
 
-    public function __construct()
+    public function __construct(Validator $res)
     {
-        parent::__construct("Bad Request", 400);
+        $er = implode(', ', $res->errors()->all());
+        parent::__construct($er, 400);
     }
 }
