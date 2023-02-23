7570e6948c33978004eccadccc48f5c4f7861736
k15 <krisnantobiyuh@gmail.com>
2023-02-23 13:51:25 +0700

    Feat(Lang) : Topup

--- lang/en/moota.php
+++ lang/en/moota.php
@@ -4,4 +4,5 @@ return [
     "failed-create-contract" => "Error Request to Moota Server. See log file or contact administrator",
     "invalid-bank-type" => "The :attribute is not valid",
     "invalid-mutation-type" => "The :attribute is not valid",
+    "topup-data-not-valid" => "Invalid topup data",
 ];
--- lang/id/moota.php
+++ lang/id/moota.php
@@ -4,4 +4,5 @@ return [
     "failed-create-contract" => "Terjadi Kesalahan Pada Server Moota. Lihat file log atau hubungi administrator",
     "invalid-bank-type" => ":attribute tidak tersedia",
     "invalid-mutation-type" => ":attribute tidak tersedia",
+    "topup-data-not-valid" => "Topup data tidak sesuai",
 ];
